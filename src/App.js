ed2348db03e42a6b14e4b43b48abffe73f1f2ae1
suraratkhanthong <khanthongsurarat@gmail.com>
2023-02-27 00:58:11 +0700

    edit nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
-
 import ContentApp from './comp/ContentApp';
 import HeaderApp from './comp/HeaderApp';
-//import dataContents from './data-app/DataContent';
-
 import ImgPost from './comp/ImgPost';
 
 
@@ -12,88 +9,89 @@ function App() {
   let [dataContents, setDataContents] = useState([]);
   let testData = [
     {
-      
+
       "name": "test",
       "url": "./images/item1.png",
       "title": "test1"
     },
     {
-      
+
       "name": "test",
       "url": "./images/item1.png",
       "title": "test1"
     },
     {
-      
+
       "name": "test",
       "url": "./images/item1.png",
       "title": "test1"
     },
     {
-      
+
       "name": "test",
       "url": "./images/item1.png",
       "title": "test1"
     },
     {
-      
+
       "name": "test",
       "url": "./images/item1.png",
       "title": "test1"
     }
   ]
-  
-  // async function fetchApi(){
-  //  //const urlApi = 'https://app-api-eight.vercel.app/posts';
-  //  const urlApi = 'http://localhost:4000/posts';
-  //  const res = await fetch(urlApi);
-  //  res.json()
-  //  .then(res => setDataContents(res))
-  //   .catch((err)=>{console.log("ERR : "+err)});
-  // }
-  useEffect(()=>{
-    //fetchApi()
-    setDataContents(testData)
-  },[])
-  
-  
-  let [selectImgOpen, setSelectImgOpen]= useState(null);
-  function ClickOpenImg(imgItem){
-   setSelectImgOpen(imgItem)
+
+
+  useEffect(() => {
+    const fetchApi = () => {
+      return setDataContents(testData)
+      //const urlApi = 'https://app-api-eight.vercel.app/posts';
+      //  const urlApi = 'http://localhost:4000/posts';
+      //  const res = await fetch(urlApi);
+      //  res.json()
+      //  .then(res => setDataContents(res))
+      //   .catch((err)=>{console.log("ERR : "+err)});
+    };
+    fetchApi();
+  }, [])
+
+
+  let [selectImgOpen, setSelectImgOpen] = useState(null);
+  function ClickOpenImg(imgItem) {
+    setSelectImgOpen(imgItem)
   }
-  function ClickCloseImg(){
+  function ClickCloseImg() {
     setSelectImgOpen(null)
-   }
-  
-  
-   
-   let dataElements=null
-   dataElements = dataContents.map((dataContent, index) =>{
-    return ( <ContentApp key={index} 
-    dataContent={dataContent}
-    onClickOpen={ClickOpenImg}
-      />);
+  }
+
+
+
+  let dataElements = null
+  dataElements = dataContents.map((dataContent, index) => {
+    return (<ContentApp key={index}
+      dataContent={dataContent}
+      onClickOpen={ClickOpenImg}
+    />);
   })
-   
-   let imgPost = null;
-    if(!!selectImgOpen){
-      imgPost = <ImgPost 
+
+  let imgPost = null;
+  if (!!selectImgOpen) {
+    imgPost = <ImgPost
       imgItem={selectImgOpen}
       clickCloseImg={ClickCloseImg}
-      />;
-    }
+    />;
+  }
 
-  
+
   return (
     <div className='app'>
-    <HeaderApp />
-    {/* <div className="description-app">
+      <HeaderApp />
+      {/* <div className="description-app">
       <span>บริการสักยันโดยช่างมืออาชีพ</span>
       </div> */}
-    <div className="show-img">
-    {dataElements}
-     </div>
-     {imgPost}
+      <div className="show-img">
+        {dataElements}
+      </div>
+      {imgPost}
     </div>
   );
 }
--- src/comp/HeaderApp.js
+++ src/comp/HeaderApp.js
@@ -1,76 +1,41 @@
 import './HeaderApp.css';
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function HeaderApp() {
   let [addClassIcon, setAddClassIcon] = useState("menu");
-  let [addClassNav, setAddClassNav] = useState();
+  let [addClassNav, setAddClassNav] = useState("");
   let [count, setCount] = useState(0);
-  //let [addClassMenuitems, setAddClassMenuitems] = useState("nav__menu-items");
+
+
   function ActionHamburger() {
-    
-    if(count == 0){
+    if (count === 0) {
       setAddClassIcon(addClassIcon + " opened")
       setAddClassNav("nav__menu-lists");
       setCount(count + 1);
-    }else{
+    } else {
       setAddClassIcon("menu")
-      setAddClassNav();
+      setAddClassNav("");
       setCount(count - 1);
     }
-    //setAddClassMenuitems("nav__menu-items");
-    // if (addClassIcon === "menu") {
-    //   setAddClassIcon(addClassIcon + " opened")
-    // } else {
-    //   setAddClassIcon("menu")
-    // }
-    
   }
-
-
   return (
-
     <div className='Header-app'>
       <img src="../images/logo2.png" />
       <i className="nav__menu">
-        
-
         <button className={addClassIcon} onClick={() => { ActionHamburger() }} aria-label="Main Menu">
-          <svg   viewBox="0 0 100 100">
+          <svg viewBox="0 0 100 100">
             <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
             <path className="line line2" d="M 20,50 H 80" />
             <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
           </svg>
-
         </button>
-        <ul class={addClassNav}>
-          <li class="nav__menu-items">เกี่ยวกับ</li>
-          <li class="nav__menu-items">สถานที่</li>
-          <li class="nav__menu-items">ติดต่อ</li>
-
+        <ul className={addClassNav}>
+          <li className="nav__menu-items" >เกี่ยวกับ</li>
+          <li className="nav__menu-items">สถานที่</li>
+          <li className="nav__menu-items">ติดต่อ</li>
         </ul>
-
       </i>
-
-
-
-
-
-
-
-
-      {/* <div class="nav__menu">menu
-          <ul class="nav__menu-lists nav__menu--2-lists">
-            <li class="nav__menu-items">Etiam</li>
-            <li class="nav__menu-items">Lectus</li>
-            <li class="nav__menu-items">Netus</li>
-           
-          </ul>
-        </div> */}
-
-
-
     </div>
-
   )
 }
 export default HeaderApp;   
